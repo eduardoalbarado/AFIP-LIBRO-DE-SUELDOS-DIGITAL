bc0b89503b61bea30b66bf7ac18ce2ef0ea830db
Eduardo <eduardoalbarado@gmail.com>
2019-11-26 13:28:10 -0300

    Agregar archivos de proyecto.

---
+++ Conceptos/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Conceptos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 64);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(819, 202);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 272);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(819, 206);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Convertir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 484);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(819, 206);
+            this.textBox3.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(962, 720);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Conversor de Conceptos AFIP";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Conceptos/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Conceptos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            OpenFileDialog openFileDialog1 = new OpenFileDialog
+            {
+                InitialDirectory = @"C:\",
+                Title = "Seleccione archivo Conceptos del TIEMPO",
+
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                DefaultExt = "txt",
+                Filter = "txt files (*.txt)|*.txt",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+
+                ReadOnlyChecked = true,
+                ShowReadOnly = true
+            };
+            
+            
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //textBox1.Text = openFileDialog1.FileName;
+                try
+                {
+                    textBox1.Text = File.ReadAllText(openFileDialog1.FileName, Encoding.ASCII);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("No se puedo abrir el archivo, verifique que exista y no este en uso.");
+                    throw;
+                }
+
+                try
+                {
+                    string encabezado;
+                    encabezado = "Código AFIP;Descripción;Código contribuyente;Descripción;Marca repetible;Aportes SIPA;Contribuciones SIPA;Aportes INSSJyP;Contribuciones INSSJyP;Aportes obra social;Contribuciones obra social;Aportes FSR;Contribuciones FSR;Aportes RENATEA;Contribuciones RENATEA;Contribuciones AAFF;Contribuciones FNE;Contribuciones LRT;Aportes diferenciales;Aportes especiales" + "\r\n";
+                    string convertido;
+                    string convertido2 ="";
+                    convertido = encabezado;
+
+                    string lines;
+                    lines = textBox1.Text;
+
+                    string[] array = lines.Split("\r\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+
+                    foreach (String item in array)
+                    {
+                        convertido = convertido + item.Substring(0, 6);     // codigo afip
+                        convertido = convertido + ";";
+                        convertido = convertido + item.Substring(16, 20).Trim();   // texto concepto afip
+                        convertido = convertido + ";";
+                        convertido = convertido + item.Substring(0, 9);     // codigo tiempo
+                        convertido = convertido + ";";
+                        convertido = convertido + item.Substring(16, 20).Trim();   // texto concepto tiempo
+                        convertido = convertido + ";";
+                        convertido = convertido + item.Substring(166, 20).Trim().Replace(" ","");   // nomeclarura
+
+                        convertido = convertido + "\r\n";
+                        //######################################################
+                        convertido2 = convertido2 + item.Substring(0, 6);     // codigo afip
+                        convertido2 = convertido2 + (item.Substring(0, 9) + string.Concat(Enumerable.Repeat(" ", 9))).Substring(0,10);     // codigo tiempo
+                        convertido2 = convertido2 + (item.Substring(16, 20).Trim()+ string.Concat(Enumerable.Repeat(" ", 150))).Substring(0,150);    // texto concepto tiempo
+                        convertido2 = convertido2 + item.Substring(166, 20).Trim()+ string.Concat(Enumerable.Repeat(" ", 9));   // nomeclarura
+
+                        convertido2 = convertido2 + "\r\n";
+
+                        Console.WriteLine(item);
+                        
+                    }
+                    textBox2.Text = convertido;
+                    textBox3.Text = convertido2;
+                    File.WriteAllText(openFileDialog1.FileName.ToUpper().Replace(".TXT", "_subirAfip.txt"), textBox3.Text);
+
+
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Error en la conversión, verifique que el formato es el correcto.");
+                    throw;
+                }
+                
+            }
+        }
+    }
+}
