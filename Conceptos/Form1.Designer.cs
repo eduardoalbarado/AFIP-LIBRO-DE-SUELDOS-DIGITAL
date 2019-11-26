6e4063ab3bb279a15d01520378acd431a157bb2a
Eduardo <eduardoalbarado@gmail.com>
2019-11-26 13:50:41 -0300

    Valida que el archivo de origen no sea el resltado de una conversión

--- Conceptos/Form1.Designer.cs
+++ Conceptos/Form1.Designer.cs
@@ -36,43 +36,45 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 64);
+            this.textBox1.Location = new System.Drawing.Point(5, 64);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
             this.textBox1.Size = new System.Drawing.Size(819, 202);
             this.textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(12, 272);
+            this.textBox2.Location = new System.Drawing.Point(619, 22);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(819, 206);
+            this.textBox2.Size = new System.Drawing.Size(51, 16);
             this.textBox2.TabIndex = 1;
+            this.textBox2.Visible = false;
             // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(13, 22);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(125, 23);
             this.button1.TabIndex = 2;
-            this.button1.Text = "Convertir";
+            this.button1.Text = "Convertir Conceptos";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(12, 484);
+            this.textBox3.Location = new System.Drawing.Point(5, 272);
             this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(819, 206);
+            this.textBox3.Size = new System.Drawing.Size(819, 259);
             this.textBox3.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(962, 720);
+            this.ClientSize = new System.Drawing.Size(828, 536);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
--- Conceptos/Form1.cs
+++ Conceptos/Form1.cs
@@ -51,56 +51,76 @@ namespace Conceptos
                     MessageBox.Show("No se puedo abrir el archivo, verifique que exista y no este en uso.");
                     throw;
                 }
-
-                try
+                if (openFileDialog1.FileName.Contains("_subirAfip"))
                 {
-                    string encabezado;
-                    encabezado = "Código AFIP;Descripción;Código contribuyente;Descripción;Marca repetible;Aportes SIPA;Contribuciones SIPA;Aportes INSSJyP;Contribuciones INSSJyP;Aportes obra social;Contribuciones obra social;Aportes FSR;Contribuciones FSR;Aportes RENATEA;Contribuciones RENATEA;Contribuciones AAFF;Contribuciones FNE;Contribuciones LRT;Aportes diferenciales;Aportes especiales" + "\r\n";
-                    string convertido;
-                    string convertido2 ="";
-                    convertido = encabezado;
-
-                    string lines;
-                    lines = textBox1.Text;
-
-                    string[] array = lines.Split("\r\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
-
-
-                    foreach (String item in array)
+                    MessageBox.Show("ERROR: El archivo seleccionado aparentemente es el resultado de una conversión.");
+                }
+                else
+                {
+                    
+                    try
                     {
-                        convertido = convertido + item.Substring(0, 6);     // codigo afip
-                        convertido = convertido + ";";
-                        convertido = convertido + item.Substring(16, 20).Trim();   // texto concepto afip
-                        convertido = convertido + ";";
-                        convertido = convertido + item.Substring(0, 9);     // codigo tiempo
-                        convertido = convertido + ";";
-                        convertido = convertido + item.Substring(16, 20).Trim();   // texto concepto tiempo
-                        convertido = convertido + ";";
-                        convertido = convertido + item.Substring(166, 20).Trim().Replace(" ","");   // nomeclarura
+                        string encabezado;
+                        encabezado = "Código AFIP;Descripción;Código contribuyente;Descripción;Marca repetible;Aportes SIPA;Contribuciones SIPA;Aportes INSSJyP;Contribuciones INSSJyP;Aportes obra social;Contribuciones obra social;Aportes FSR;Contribuciones FSR;Aportes RENATEA;Contribuciones RENATEA;Contribuciones AAFF;Contribuciones FNE;Contribuciones LRT;Aportes diferenciales;Aportes especiales" + "\r\n";
+                        string convertido;
+                        string convertido2 = "";
+                        convertido = encabezado;
 
-                        convertido = convertido + "\r\n";
-                        //######################################################
-                        convertido2 = convertido2 + item.Substring(0, 6);     // codigo afip
-                        convertido2 = convertido2 + (item.Substring(0, 9) + string.Concat(Enumerable.Repeat(" ", 9))).Substring(0,10);     // codigo tiempo
-                        convertido2 = convertido2 + (item.Substring(16, 20).Trim()+ string.Concat(Enumerable.Repeat(" ", 150))).Substring(0,150);    // texto concepto tiempo
-                        convertido2 = convertido2 + item.Substring(166, 20).Trim()+ string.Concat(Enumerable.Repeat(" ", 9));   // nomeclarura
+                        string lines;
+                        lines = textBox1.Text;
 
-                        convertido2 = convertido2 + "\r\n";
+                        string[] array = lines.Split("\r\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
 
-                        Console.WriteLine(item);
-                        
+
+                        foreach (String item in array)
+                        {
+                            convertido = convertido + item.Substring(0, 6);     // codigo afip
+                            convertido = convertido + ";";
+                            convertido = convertido + item.Substring(16, 20).Trim();   // texto concepto afip
+                            convertido = convertido + ";";
+                            convertido = convertido + item.Substring(0, 9);     // codigo tiempo
+                            convertido = convertido + ";";
+                            convertido = convertido + item.Substring(16, 20).Trim();   // texto concepto tiempo
+                            convertido = convertido + ";";
+                            convertido = convertido + item.Substring(166, 20).Trim().Replace(" ", "");   // nomeclarura
+
+                            convertido = convertido + "\r\n";
+                            //######################################################
+                            convertido2 = convertido2 + item.Substring(0, 6);     // codigo afip
+                            convertido2 = convertido2 + (item.Substring(0, 9) + string.Concat(Enumerable.Repeat(" ", 9))).Substring(0, 10);     // codigo tiempo
+                            convertido2 = convertido2 + (item.Substring(16, 20).Trim() + string.Concat(Enumerable.Repeat(" ", 150))).Substring(0, 150);    // texto concepto tiempo
+                            convertido2 = convertido2 + item.Substring(166, 20).Trim() + string.Concat(Enumerable.Repeat(" ", 9));   // nomeclarura
+
+                            convertido2 = convertido2 + "\r\n";
+
+                            Console.WriteLine(item);
+
+                        }
+                        textBox2.Text = convertido;
+                        textBox3.Text = convertido2;
+                        try
+                        {
+                            File.WriteAllText(openFileDialog1.FileName.ToUpper().Replace(".TXT", "_subirAfip.txt"), textBox3.Text);
+                            MessageBox.Show("Convesión Exitosa, busque el nuevo archivo en la misma ubicacion que el archivo de origen.");
+
+                        }
+                        catch (Exception)
+                        {
+                            MessageBox.Show("ATENCION: Conversión exitosa, no se pudo generar nuevo archivo txt.");
+                            throw;
+                        }
+
+
+
                     }
-                    textBox2.Text = convertido;
-                    textBox3.Text = convertido2;
-                    File.WriteAllText(openFileDialog1.FileName.ToUpper().Replace(".TXT", "_subirAfip.txt"), textBox3.Text);
-
+                    catch (Exception)
+                    {
+                        MessageBox.Show("Error en la conversión, verifique que el formato es el correcto.");
+                        throw;
+                    }
 
                 }
-                catch (Exception)
-                {
-                    MessageBox.Show("Error en la conversión, verifique que el formato es el correcto.");
-                    throw;
-                }
+
                 
             }
         }
